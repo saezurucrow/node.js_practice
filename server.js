6d4c36dbb6da79bb2092636dee8222ce9633a761
saezurucrow <usobukuhito@gmail.com>
2020-04-24 21:02:59 +0900

    本当のリクエストハンドラへのルーティング

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 const http = require("http");
 const url = require("url");
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     const pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, {
       "Content-Type": "text/plain"
