536793cbb14e80ec7d1141ad6842998fb36111c1
saezurucrow <usobukuhito@gmail.com>
2020-04-27 21:32:21 +0900

    うまくいかない方法

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ function start(route, handle) {
     const pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname);
-
     response.writeHead(200, {
       "Content-Type": "text/plain"
     });
-    response.write("Hello World");
+    const content = route(handle, pathname)
+    response.write(content);
     response.end();
   }
 
