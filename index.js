03d9802545080e45b77b4ca19df581966c87935c
saezurucrow <usobukuhito@gmail.com>
2020-05-02 22:20:52 +0900

    完成

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const server = require("./server");
 const router = require("./router");
 const requestHandlers = require("./requestHandlers");
 
-const handle = {}
+const handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
