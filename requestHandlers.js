a21023681a756a160edc886298ed453bf73712e0
saezurucrow <usobukuhito@gmail.com>
2020-05-02 22:05:20 +0900

    ノンブロッキング操作で応答するリクエストハンドラ

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,28 @@
 const exec = require("child_process").exec;
 
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
-  const content = "empty";
 
-  exec("ls -lah", function (error, stdout, stderr) {
-    content = stdout;
-  });
-
-  return content;
+  exec("find /", {
+      timeout: 10000,
+      maxBuffer: 20000 * 1024
+    },
+    function (error, stdout, stderr) {
+      response.writeHead(200, {
+        "Content-Type": "text/plain"
+      });
+      response.write(stdout);
+      response.end();
+    });
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
